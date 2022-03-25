1843edb7e2c2005da29b7ed7d2ea0844063d5489
Diogo15 <luisdiegore@hotmail.com>
2022-03-25 10:52:29 -0600

    Login activity content added

--- app/src/main/java/com/socialmediagamer/LoginActivity.kt
+++ app/src/main/java/com/socialmediagamer/LoginActivity.kt
@@ -1,11 +1,38 @@
 package com.socialmediagamer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.socialmediagamer.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    fun hacelogin(){
+
+        val user = binding.txtUser.text.toString()
+        val clave = binding.txtPass.text.toString()
+
+        auth.signInWithEmailAndPassword(user,clave)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+        //bypass login
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
+    }
+
 }
