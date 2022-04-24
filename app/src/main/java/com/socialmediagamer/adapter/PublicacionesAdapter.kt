671d6dcd5bc73e057b1821ae683d893da1524b86
kshum <kathashuma@gmail.com>
2022-04-23 20:21:10 -0600

    Mostrar categorias a las publicaciones

--- app/src/main/java/com/socialmediagamer/adapter/PublicacionesAdapter.kt
+++ app/src/main/java/com/socialmediagamer/adapter/PublicacionesAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.socialmediagamer.R
 import com.socialmediagamer.databinding.PublicacionFilaBinding
 import com.socialmediagamer.model.Publicacion
 
@@ -16,6 +17,22 @@ class PublicacionesAdapter: RecyclerView.Adapter<PublicacionesAdapter.Publicacio
         fun bind(publicacion: Publicacion){
             itemBinding.txtPostTitle.text = publicacion.titulo
             itemBinding.txtPostDesc.text = publicacion.descripcion
+            if(publicacion.categoria=="Nintendo"){
+                itemBinding.imageButton2.setImageResource(R.drawable.ic_nintendo)
+                itemBinding.imageButton2.setBackgroundColor("#A81414")
+            }
+            if(publicacion.categoria=="PC"){
+                itemBinding.imageButton2.setImageResource(R.drawable.icon_nintendo)
+                itemBinding.imageButton2.setBackgroundColor("#CCCCCC")
+            }
+            if(publicacion.categoria=="PS4"){
+                itemBinding.imageButton2.setImageResource(R.drawable.ic_ps4)
+                itemBinding.imageButton2.setBackgroundColor("#000080")
+            }
+            if(publicacion.categoria=="Xbox"){
+                itemBinding.imageButton2.setImageResource(R.drawable.ic_nintendo)
+                itemBinding.imageButton2.setBackgroundColor("#008000")
+            }
             /*
             itemBinding.vistaFila.setOnClickListener{
                 val action = PublicacionesViewHolder.actionPublicacionFragmentToUpdatePublicacionFragment(publicacion)
